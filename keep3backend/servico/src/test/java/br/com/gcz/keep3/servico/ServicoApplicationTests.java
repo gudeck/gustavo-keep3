750881ce44854f1dd23386242acd029fa2671937
gudeck <guzuc@outlook.com>
2022-01-15 11:38:27 -0300

    iniciando projeto

---
+++ keep3backend/servico/src/test/java/br/com/gcz/keep3/servico/ServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gcz.keep3.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
