d34016a87283afd34970941bc4cc75d8a307de62
gudeck <guzuc@outlook.com>
2022-01-15 17:17:23 -0300

    atividade 1 - adicionando segundo microsserviço

---
+++ keep3backend/documento/src/main/java/br/com/gcz/keep3/documento/DocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gcz.keep3.documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentoApplication.class, args);
+    }
+
+}
---
+++ keep3backend/documento/src/test/java/br/com/gcz/keep3/documento/DocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gcz.keep3.documento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
